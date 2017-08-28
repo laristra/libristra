76176692c45227201962019565b42ba993b21838
Timothy Kelley <tkelley@lanl.gov>
2017-08-28 10:58:51 -0600

    Fixed documentation

--- ristra/static_warning.h
+++ ristra/static_warning.h
@@ -11,20 +11,20 @@
 // two steps give macros like __LINE__ a chance to be evaluated
 #define RISTRA_CAT(x,y) RISTRA_CAT1(x,y)
 #define RISTRA_CAT1(x,y) x##y
-
 #define RISTRA_DECO(c,m) #c":"#m
 
 /**\brief This is a klunky way of generating compile time messages.
  *
  * This can be useful in tracing the path through compile-time template
- * deduction and overload resolution.
+ * deduction and overload resolution. It should be useable at namespace
+ * and function scope.
  *
  * It works by compile-time evaluating a condition: if the condition is false
  * then it invokes a deprecated method that does nothing. The deprecated
  * attribute is standard C++11/14; most compilers have a mechanism to print
  * out a warning.
  *
- * Use this sparingly, for example when actively debugging a template issue.
+ * Use this sparingly: for example when actively debugging a template issue.
  */
 #define static_warning(cond, msg)                \
   struct RISTRA_CAT(Static_warning,__LINE__) {                   \
