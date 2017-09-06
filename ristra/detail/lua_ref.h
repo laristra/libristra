ba85449bc644e0d0e031566a8d6a86339cf2dc3e
Timothy Kelley <tkelley@lanl.gov>
2017-09-06 11:18:17 -0600

    Added ability to push std::array<T,N>; this changes how we count args.

--- ristra/detail/lua_ref.h
+++ ristra/detail/lua_ref.h
@@ -30,44 +30,86 @@ namespace detail{
 /// \brief Push an integer onto the stack.
 /// \param [in] s  The lua state to push a value to.
 /// \param [in] i  The integer to push.
-inline void lua_push(lua_State * s, int i)
-{ lua_pushinteger( s, i ); }
+inline size_t lua_push(lua_State * s, int i)
+{
+  lua_pushinteger( s, i );
+  return 1;
+}
 
 /// \brief Push a long long onto the stack.
 /// \param [in] s  The lua state to push a value to.
 /// \param [in] i  The long long to push.
-inline void lua_push(lua_State * s, long long i)
-{ lua_pushinteger( s, i ); }
+inline size_t lua_push(lua_State * s, long long i)
+{
+  lua_pushinteger( s, i );
+  return 1;
+}
 
 /// \brief Push a float onto the stack.
 /// \param [in] s  The lua state to push a value to.
 /// \param [in] x  The float to push.
-inline void lua_push(lua_State * s, float x)
-{ lua_pushnumber( s, x ); }
+inline size_t lua_push(lua_State * s, float x)
+{
+  lua_pushnumber( s, x );
+  return 1;
+}
 
 /// \brief Push a double onto the stack.
 /// \param [in] s  The lua state to push a value to.
 /// \param [in] x  The double to push.
-inline void lua_push(lua_State * s, double x)
-{ lua_pushnumber( s, x ); }
+inline size_t lua_push(lua_State * s, double x)
+{
+  lua_pushnumber( s, x );
+  return 1;
+}
 
 /// \brief Push a boolean onto the stack.
 /// \param [in] s  The lua state to push a value to.
 /// \param [in] b  The boolean to push.
-inline void lua_push(lua_State * s, bool b)
-{ lua_pushboolean( s, b ); }
+inline size_t lua_push(lua_State * s, bool b)
+{
+  lua_pushboolean( s, b );
+  return 1;
+}
 
 /// \brief Push a character array onto the stack.
 /// \param [in] s  The lua state to push a value to.
 /// \param [in] str  The character array to push.
-inline void lua_push(lua_State * s, const char * str)
-{ lua_pushstring( s, str ); }
+inline size_t lua_push(lua_State * s, const char * str)
+{
+  lua_pushstring( s, str );
+  return 1;
+}
 
 /// \brief Push a std::string onto the stack.
 /// \param [in] s  The lua state to push a value to.
 /// \param [in] str  The string to push.
-inline void lua_push(lua_State * s, const std::string & str)
-{ lua_pushlstring( s, str.c_str(), str.size() ); }
+inline size_t lua_push(lua_State * s, const std::string & str)
+{
+  lua_pushlstring( s, str.c_str(), str.size() );
+  return 1;
+}
+/// \brief Push a std::array onto the stack.
+/// \param [in] s  The lua state to push a value to.
+/// \param [in] str  The array to push.
+template <size_t N>
+inline size_t lua_push(lua_State * s, const std::array<double,N> & arr)
+{
+  // Caller always checks for 1, we must check for the rest
+  auto ret = lua_checkstack(s, N-1);
+  if ( !ret ) {
+    std::ostringstream ss;
+    ss << "Cannot grow stack " << (N - 1) << " slots operating on element \""
+       << "\"." << std::endl
+       << "Current stack size is " << lua_gettop(s) << ".";
+    throw_runtime_error(ss.str());
+  }
+  // push each element of the array
+  for(size_t i = 0; i < N; ++i){
+    lua_push( s, arr[i]);
+  }
+  return N;
+}
 /// \}
 
 ////////////////////////////////////////////////////////////////////////////////
