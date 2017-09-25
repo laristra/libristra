06f3179b5398cde34e850a63944b244a0c0022b3
Timothy Kelley <tkelley@lanl.gov>
2017-09-25 16:53:28 -0600

    Removed input_engine singleton logic; made clear that the types here are just defaults.

--- ristra/inputs.h
+++ ristra/inputs.h
@@ -9,6 +9,7 @@
 
 #include "input_engine.h"
 #include "detail/default_input_traits.h"
+#include "init_value.h"
 
 // Configure input_engine macros from build system here:
 #ifdef RISTRA_DIMENSION
@@ -20,19 +21,7 @@ constexpr uint8_t default_dim = 2;
 namespace ristra{
 
 using input_traits = detail::default_input_traits<default_dim>;
-using input_engine = input_engine_t<input_traits>;
-
-input_engine &get_input_engine(){
-  static input_engine the_engine;
-  return the_engine;
-}
-
-/**\brief trigger the input engine to resolve its inputs from
- * the sources registered to it.
- *
- * \return whether all inputs were successfully resolved.
- */
-bool resolve_inputs(){ return get_input_engine().resolve_inputs(); }
+using default_input_engine = input_engine_t<input_traits>;
 
 } // ristra::
 
