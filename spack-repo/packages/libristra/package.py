a86f2a22a25bc55fd97d5264cc731c84ee0a92f5
Karen Chung-Yen Tsai <ktsai@lanl.gov>
2019-12-17 15:29:03 -0700

    adding gitlab ci with spack

---
+++ spack-repo/packages/libristra/package.py
@@ -0,0 +1,36 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+
+
+class Libristra(CMakePackage):
+    '''Libristra is a support library for other ristra codes.
+    '''
+    git      = 'https://github.com/laristra/libristra.git'
+
+    version('master', branch='master', submodules=False, preferred=True)
+
+    variant('build_type', default='Release', values=('Debug', 'Release'),
+            description='The build type to build', multi=False)
+
+    depends_on('cmake@3.12:',  type='build')
+    # Requires cinch > 1.0 due to cinchlog installation issue
+    #depends_on('cinch@1.01:', type='build')
+    depends_on('boost@1.70.0: cxxstd=14 +program_options')
+    depends_on('lua@5.3.5')
+
+    def cmake_args(self):
+        spec = self.spec
+        options = []
+        #options.append('-DCINCH_SOURCE_DIR=' + spec['cinch'].prefix)
+
+        if self.run_tests:
+            options.append('-DENABLE_UNIT_TESTS=ON')
+        else:
+            options.append('-DENABLE_UNIT_TESTS=OFF')
+
+        return options
