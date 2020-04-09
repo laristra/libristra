806e0a8e95206d4065b9a085d634498f4340c3c0
Karen Chung-Yen Tsai <ktsai@lanl.gov>
2020-04-09 16:46:56 -0600

    Update package.py

--- spack-repo/packages/libristra/package.py
+++ spack-repo/packages/libristra/package.py
@@ -19,7 +19,7 @@ class Libristra(CMakePackage):
             description='The build type to build', multi=False)
     variant('paraview', default=False,
             description='Enable ParaView')
-    variant('cinch', default=False,
+    variant('cinch', default=True,
             description='Enable External Cinch')
 
     depends_on('cmake@3.12:',  type='build')
